2d96bdd1550e10aca97ffb2b45f28bf1d45bbc8f
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2021-10-04 03:57:20 +0300

    Wilsonin algoritmin korjaus
    Algoritmi näyttäisi toimivan odotetusti. Ohessa rakennettu
    testi labyrintin luontia varten.

--- src/main/kotlin/algo/Wilson.kt
+++ src/main/kotlin/algo/Wilson.kt
@@ -1,27 +1,24 @@
 package luolastogeneraattori.algo
 
 import luolastogeneraattori.util.Ruutu
-import kotlin.random.Random
 
 class Wilson {
     private var kaydyt = arrayOf<Array<Ruutu>>()
-    private val ylos = 1
-    private val alas = 2
-    private val oikea = 4
-    private val vasen = 8
-    private var suunnat = arrayOf(ylos, alas, oikea, vasen)
-    private var dx = mapOf(ylos to 0, alas to 0, oikea to 1, vasen to -1)
-    private var dy = mapOf(ylos to -1, alas to 1, oikea to 0, vasen to 0)
-    private var vastakkainen = mapOf(oikea to vasen, vasen to oikea, ylos to alas, alas to ylos)
+    private var suunnat = arrayOf("ylos", "alas", "oikea", "vasen")
+    private var vastakkainen = mapOf("oikea" to "vasen", "vasen" to "oikea", "ylos" to "alas", "alas" to "ylos")
+    private var dx = mapOf("ylos" to 0, "alas" to 0, "oikea" to 1, "vasen" to -1)
+    private var dy = mapOf("ylos" to -1, "alas" to 1, "oikea" to 0, "vasen" to 0)
 
     /**
      * Funktio 2D-ruudukon rakentamiseksi, jossa lopulta ilmenee labyrinttiin lisätyt ruudut
      */
     fun rakennaRuudukko(leveys: Int, korkeus: Int): Array<Array<Ruutu>> {
         var ruudukko = arrayOf<Array<Ruutu>>()
-        for (n in 1..leveys) {
+        // Rivejä (korkeus)
+        for (n in 1..korkeus) {
             var rivi = arrayOf<Ruutu>()
-            for (j in 1..korkeus) {
+            // Rivissä ruutuja (leveys)
+            for (j in 1..leveys) {
                 rivi += Ruutu(null, 0)
             }
             ruudukko += rivi
@@ -33,10 +30,10 @@ class Wilson {
     /**
      * Alustetaan erillinen taulukko käyntien seuraamiseksi
      */
-    fun alustaKaydyt(ruudukko: Array<Array<Ruutu>>) {
-        for (n in 0 until ruudukko[0].size) {
+    private fun alustaKaydyt(ruudukko: Array<Array<Ruutu>>) {
+        for (n in 1..ruudukko.size) {
             var rivi = arrayOf<Ruutu>()
-            for (j in ruudukko.indices) {
+            for (j in 1..ruudukko[0].size) {
                 val r = Ruutu(null, 0)
                 rivi += r
             }
@@ -59,29 +56,33 @@ class Wilson {
     /**
      * Reitin tulostamista varten
      */
-    private fun debugReitti(reitti: Triple<Int, Int, Int>) {
-        println("Reitin debuggaus::")
-        println("Koordinaatit rivi: ${reitti.first}, sarake: ${reitti.second}, suunta: ${reitti.third}")
+    private fun debugReitti(reitti: Triple<Int, Int, String>) {
+        println("Reitin debuggaus.")
+        println("Koordinaatit rivi: ${reitti.first+1}, sarake: ${reitti.second+1}, suunta: ${reitti.third}")
     }
 
     /**
      * Wilsonin algoritmin satunnaiskävely
      */
-    private fun randomKavely(ruudukko: Array<Array<Ruutu>>): Array<Triple<Int, Int, Int>> {
-        testi@ while (true) {
-            // X-akselin koordinaatti (sarake)
-            var cx = Random.nextInt(0, ruudukko[0].size - 1)
+    private fun randomKavely(ruudukko: Array<Array<Ruutu>>): Array<Triple<Int, Int, String>> {
+        while (true) {
             // Y-akselin koordinaatti (rivi)
-            var cy = Random.nextInt(0, ruudukko.size - 1)
+            var cy = (ruudukko.indices).random()
+            // X-akselin koordinaatti (sarake)
+            var cx = (ruudukko[0].indices).random()
 
-            println("Satunnaisalkukoordinaatit kävelylle: \n X: $cx \n Y: $cy\nSatunnaisarvo: ${ruudukko[cy][cx].arvo}")
+
+            println("Kävely alkaa pisteestä: \n rivi: ${cy+1} \n sarake: ${cx+1}")
 
             // Jos ruudukon arvo on jo labyrintissa, haetaan uusi koordinaatti
-            if (ruudukko[cy][cx].arvo != 0) continue@testi
+            if (ruudukko[cy][cx].arvo != 0) {
+                println("Osuttiin suoraan reittiin - haetaan uusi alkukoordinaatti.")
+                continue
+            }
 
             // Otetaan reitin alkukoordinaatit talteen
-            val alkuX = cx
             val alkuY = cy
+            val alkuX = cx
 
             var randomKavely: Boolean
 
@@ -90,48 +91,46 @@ class Wilson {
                 randomKavely = false
                 suunnat.shuffle()
                 for (suunta in suunnat) {
-                    // println("Suunta: $suunta")
-                    val nx = cx + dx[suunta]!!
+                    // Lasketaan suunnan osoittaman ruudun koordinaatit
                     val ny = cy + dy[suunta]!!
+                    val nx = cx + dx[suunta]!!
 
-                    // println("NX: $nx \n NY: $ny")
+                    println("Satunnaiskävelyn osoittama ruutu: ${ny+1}, ${nx+1}")
 
                     // Onko naapuriruutu validi (ruudukon sisällä)
                     if (nx >= 0 && ny >= 0 && ny < ruudukko.size && nx < ruudukko[ny].size) {
                         // Validin naapurin löydyttyä asetetaan se poistumisvektoriksi
                         kaydyt[cy][cx].suunta = suunta
 
-                        // Jos naapuriruudussa on jo käyty, niin poistutaan kävelystä
-                        if (ruudukko[ny][nx].arvo != 0) {
-                            break
-                        } else {
+                        // Jos naapuriruudussa on jo käyty, niin poistutaan kävelystä.
+                        if (ruudukko[ny][nx].arvo == 0) {
                             // Muussa tapauksessa jatketaan kävelyä (kunnes törmätään jo käytyyn ruutuun)
                             // Asetetaan uudet koordinaatit ja indikoidaan kävelyn jatkuvuus
-                            cx = nx
                             cy = ny
+                            cx = nx
                             randomKavely = true
-                            break
                         }
+                        break
+                    } else {
+                        println("Osuttiin yli ruudukkorajojen. Haetaan uusi koordinaatti")
                     }
                 }
             } while (randomKavely)
 
-            // println("Poistuttiin kävelystä")
+            println("Osuttiin reitillä olevaan ruutuun / maaliruutuun")
 
             // Alustetaan reittimonikkolista alkaen alkukoordinaatista
-            var reitti = arrayOf<Triple<Int, Int, Int>>()
+            var reitti = arrayOf<Triple<Int, Int, String>>()
             var x = alkuX
             var y = alkuY
 
             // Navigoidaan aina reitin seuraavaan ruutuun apulistojen avulla
             while (true) {
-                if (kaydyt[y][x].suunta !== null) {
-                    val dir: Int? = kaydyt[y][x].suunta
-                    if (dir !== null) {
-                        reitti += Triple(y, x, dir)
-                        x += dx[dir]!!
-                        y += dy[dir]!!
-                    }
+                val suunta = kaydyt[y][x].suunta
+                if (suunta !== null) {
+                    reitti += Triple(y, x, suunta)
+                    y += dy[suunta]!!
+                    x += dx[suunta]!!
                 } else {
                     break
                 }
@@ -146,9 +145,12 @@ class Wilson {
      * Algoritmin ajaminen niin kauan kunnes kaikki ruudut on käyty läpi
      */
     fun muunnaLabyrintiksi(ruudukko: Array<Array<Ruutu>>): Array<Array<Ruutu>> {
+        // Alustetaan erillinen käyntejä seuraava taulukko
+        alustaKaydyt(ruudukko)
+
         // Valitaan loppupisteeksi satunnainen piste
-        val satunnaisX = Random.nextInt(0, ruudukko.size - 1)
-        val satunnaisY = Random.nextInt(0, ruudukko[0].size - 1)
+        val satunnaisX = (ruudukko.indices).random()
+        val satunnaisY = (ruudukko[0].indices).random()
 
         // Asetetaan loppupisteen arvoksi 2, joka indikoi maaliruutua
         println("Maaliruuduksi on valittu koordinaatit: rivi: ${satunnaisY+1}, sarake: ${satunnaisX+1}")
@@ -158,24 +160,26 @@ class Wilson {
         var ruutujaJaljella = ruudukko.size * ruudukko[0].size - 1
         while (ruutujaJaljella > 0) {
             for (it in randomKavely(ruudukko)) {
-                debugReitti(it)
-                // Suunnan osoittama y-koordinaatti (rivi)
-                val ny = it.first + dy[it.third]!!
-                // Suunnan osoittama x-koordinaatti (sarake)
-                val nx = it.second + dx[it.third]!!
+                if (ruudukko[it.first][it.second].arvo == 0) {
+                    debugReitti(it)
+                    // Suunnan osoittama y-koordinaatti (rivi)
+                    val ny = it.first + dy[it.third]!!
+                    // Suunnan osoittama x-koordinaatti (sarake)
+                    val nx = it.second + dx[it.third]!!
 
-                ruudukko[it.first][it.second].suunta = it.third
-                ruudukko[it.first][it.second].arvo = 1
-                ruudukko[ny][nx].suunta = vastakkainen[it.third]
-                ruudukko[ny][nx].arvo = 1
-                ruutujaJaljella -= 1
-                println("Ruutuja jäljellä: $ruutujaJaljella")
+                    ruudukko[it.first][it.second].suunta = it.third
+                    ruudukko[it.first][it.second].arvo = 1
+                    ruudukko[ny][nx].suunta = vastakkainen[it.third]
+                    ruutujaJaljella -= 1
+                    println("Ruutuja jäljellä: $ruutujaJaljella")
 
-                if (ruutujaJaljella == 0) {
-                    break
+                    if (ruutujaJaljella == 0) {
+                        break
+                    }
+
+                    debugRuudukko(ruudukko)
                 }
 
-                debugRuudukko(ruudukko)
             }
         }
 
--- src/test/kotlin/algo/WilsonTest.kt
+++ src/test/kotlin/algo/WilsonTest.kt
@@ -6,10 +6,10 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.Assertions.*
 
 internal class WilsonTest {
-    private val wilson = Wilson()
-
     @Test
     fun ruudukonRakennusJaDebug() {
+        // Testi ruudukonrakennusmetodille
+        val wilson = Wilson()
         val ruudukko = wilson.rakennaRuudukko(3, 3)
         assertEquals(ruudukko.size, 3)
         assertEquals(ruudukko[0].size, 3)
@@ -18,10 +18,24 @@ internal class WilsonTest {
 
     @Test
     fun kaveleeLoppuun() {
+        // Yksinkertainen testi algoritmin läpivedolle. AssertNotNull on enemmänkin muodollisuus, eikä mittaa mitään
+        // palautusarvoon liittyvää. Epäonnistuessa tämä testi jäisi ikuiseen luuppiin.
         val wilson = Wilson()
         val ruudukko = wilson.rakennaRuudukko(3, 3)
-        wilson.alustaKaydyt(ruudukko)
         val laby = wilson.muunnaLabyrintiksi(ruudukko)
         assertNotNull(laby)
+    }
+
+    @Test
+    fun labyrinttiKaikistaRuuduista() {
+        // Tarkistaa, ettei lopulliseen ruudukkoon jää yhtään ruutua, jonka arvo olisi 0
+        val wilson = Wilson()
+        val ruudukko = wilson.rakennaRuudukko(5, 5)
+        val laby = wilson.muunnaLabyrintiksi(ruudukko)
+        for (rivi in laby) {
+            for (ruutu in rivi) {
+                assertNotEquals(ruutu.arvo, 0)
+            }
+        }
     }
 }
