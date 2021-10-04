708689179cade0bc95cf6e9c49f87c3c4f21afb1
Benjamin Blinnikka <benjamin.blinnikka@hotmail.com>
2021-10-04 03:58:27 +0300

    Ohjelman käytettävyyden parantelua
    Nyt käyttäjä voi syöttää ruudukon koot, johon labyrintti luodaan.

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,7 +2,26 @@ package luolastogeneraattori
 
 import luolastogeneraattori.algo.Wilson
 import luolastogeneraattori.util.Ruutu
+import java.lang.NumberFormatException
 import kotlin.system.measureTimeMillis
+
+fun tarkistaLukuSyote(nimi: String, arvovali: IntRange): Int {
+    var haluttuSyote: Int
+    while (true) {
+        print("Syötä ruudukon $nimi arvoväliltä $arvovali: ")
+        val syote = readLine()
+        try {
+            haluttuSyote = Integer.valueOf(syote)
+            if (haluttuSyote in arvovali) {
+                return haluttuSyote
+            } else {
+                println("Syötetty luku ei kuulu arvoväliin $arvovali")
+            }
+        } catch (e: NumberFormatException) {
+            println("Arvon $nimi tulee olla kokonaisluku")
+        }
+    }
+}
 
 fun main() {
     /* Ohjelmarunkoa:
@@ -12,11 +31,11 @@ fun main() {
         4. Algoritmi muodostaa ruudukosta käytävistön. Näytetään lopputulos käyttäjälle ajan kera.
      */
     val w = Wilson()
-    val leveys = 4
-    val korkeus = 4
+    val leveys = tarkistaLukuSyote("leveys", 3..10)
+    val korkeus = tarkistaLukuSyote("korkeus", 3..10)
     val ruudukko = w.rakennaRuudukko(leveys, korkeus)
-    w.alustaKaydyt(ruudukko)
-    println("Alustettu ruudukko leveydellä 3, korkeudella 3")
+    println("Alustettu ruudukko leveydellä $leveys, korkeudella $korkeus")
+    w.debugRuudukko(ruudukko)
     var laby: Array<Array<Ruutu>>
     val kesto = measureTimeMillis {
         laby = w.muunnaLabyrintiksi(ruudukko)
